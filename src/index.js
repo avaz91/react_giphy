2eda5d6611afb0cd8a5243793d48e2c186581d4d
avaz91 <avaz91@hotmail.com>
2019-05-30 17:41:34 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios'
+var gifs = []
 
-ReactDOM.render(<App />, document.getElementById('root'));
+axios.get("https://api.giphy.com/v1/gifs/search?api_key=O5LyiGH753b9EWs8TEN6LL0DLtx8wqNm&q=cat&limit=25&offset=0&rating=G&lang=en").then(function (response) {
+    gifs = response.data.data.map((v, i) => {
+        console.log(v, i)
+        return <li key={i}><img src={v.images['480w_still'].url} alt="cat"/></li>
+    })
+    ReactDOM.render(<ul>{gifs}</ul>, document.getElementById('root'));
+
+});
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
